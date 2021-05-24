c62e6e12e9fcc496368325d0a5a45ccd453e782e
Juanan Fernandez <juanan.fernandez@gmail.com>
2021-05-24 21:31:44 +0200

    Primera ruta GET

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 
 const app = express();
 
+const fileName = path.join(__dirname, 'dev-data', 'data', 'tours-simple.json');
+
+const tours = JSON.parse(fs.readFileSync(fileName));
+
 app.get('/api/v1/tours', (req, res, next) => {
 	res.status(200).json({
-		message: 'Hello from the other side...',
-		applicacion: 'Natours',
+		status: 'Success',
+		results: tours.length,
+		data: { tours: tours },
 	});
 });
 
